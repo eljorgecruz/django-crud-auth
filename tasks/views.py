e0addbeee3ea93e23804cb55732eaea0b38d49bc
eljorgecruz <jcruz42@uco.mx>
2025-04-22 16:46:32 -0600

    Feat: signin model

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from django.contrib.auth.models import User
 
@@ -37,3 +37,24 @@ def tasks(request):
 def signout(request):
     logout(request)
     return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm()
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm(),
+                'error': 'Username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
+
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm()
+        })
